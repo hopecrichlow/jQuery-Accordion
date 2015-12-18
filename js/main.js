384fd0c44a03657a379e78638381baad4f2b6dd8
Hadiya Hope-Crichlow <hopecrichlow@live.com>
2015-12-18 08:37:57 -0500

    accordion working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 import $ from 'jquery';
-import _ from 'underscore';
-import moment from 'moment';
 
 console.log('Hello, World');
+
+$('.header1').on('click', function (event){
+  console.log(this);
+  $('#paragraph1').removeClass('close').addClass('open');
+  $('#paragraph2').removeClass('open').addClass('close');
+  $('#paragraph3').removeClass('open').addClass('close');
+});
+
+$('.header2').on('click', function (event){
+  $('#paragraph2').removeClass('close').addClass('open');
+  $('#paragraph3').removeClass('open').addClass('close');
+  $('#paragraph1').removeClass('open').addClass('close');
+});
+
+$('.header3').on('click', function (event){
+  $('#paragraph1').removeClass('open').addClass('close');
+  $('#paragraph3').removeClass('close').addClass('open');
+  $('#paragraph2').removeClass('open').addClass('close');
+});
+
